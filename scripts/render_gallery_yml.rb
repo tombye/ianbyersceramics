6911f69fa0dd42820107ae3881fefc275cc4a196
tombye <tombaromba@gmail.com>
2017-06-19 14:05:31 +0100

    Render all gallery index pages

--- scripts/render_gallery_yml.rb
+++ scripts/render_gallery_yml.rb
@@ -3,10 +3,20 @@
 require 'erb'
 require_relative './site_data/gallery'
 
+galleries_dir = Pathname.new(File.expand_path('../../galleries', __FILE__))
 if ARGV[0].nil?
-  puts "No gallery slug provided."
+  puts "No gallery index provided."
 else
   slug = ARGV[0]
   gallery = SiteData::Gallery.new(slug)
-  puts gallery.render
+
+  # render gallery index into root and /artworks root
+  gallery_index_path = galleries_dir.join("#{gallery.slug}", "index.html")
+  unless gallery_index_path.exist?
+    File.open(gallery_index_path, "w+") do |f|
+      f.write(gallery.render)
+    end
+  else
+    puts "#{gallery_index_path} already exists, exiting"
+  end
 end
