47169e7a61fd3ea23fdd3058fdeb278466f9a41d
tombye <tombaromba@gmail.com>
2018-12-26 18:57:56 +0000

    Make deploy.sh fit new process
    We just push the latest to a remote folder now.
    
    This also adds the option to ignore the static
    assets, for when you're just making changes to the
    html.

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,11 @@ usage() {
   Deploy a new version to the remote host
 
   OPTIONS:
-    -d Deploy the code in the 'latest' folder on the remote host
+    -i Ignore static assets
 EOF
 }
 
-DEPLOY_LATEST=1
+IGNORE_STATIC_ASSETS=0
 
 while getopts "hd" OPTION; do
   case $OPTION in
@@ -18,8 +18,8 @@ while getopts "hd" OPTION; do
       usage
       exit 1
       ;;
-    d )
-      DEPLOY_LATEST=0
+    i )
+      IGNORE_STATIC_ASSETS=1
       ;;
   esac
 done
@@ -30,33 +30,18 @@ shift $(($OPTIND-1))
 VERSION=$( cat VERSION.txt )
 echo "Version is $VERSION"
 
-# 1. Check if version has been bumped
-if ssh webfactional "[ -d /home/tombaromba/webapp_releases/ianbyersceramics/$VERSION ]"; then
-  echo "Version $VERSION already exists."
-  VERSION_EXISTS=0
-else
-  VERSION_EXISTS=1
-fi
-
-RELEASES_DIR=/home/tombaromba/webapp_releases/ianbyersceramics
 WEBAPPS_DIR=/home/tombaromba/webapps
 
-if [ $VERSION_EXISTS -eq 1 ]; then
+echo "Building static site."
+# 2. build project
+bundle exec jekyll build
 
-  echo "Building static site."
-  # 2. build project
-  bundle exec jekyll build
+echo "Copy files to remote host"
+# 3. rsync latest to latest folder on remote host
+rsync -avz --exclude _site/public _site/ "webfactional:$WEBAPPS_DIR/ianbyersceramics"
 
+if [ $IGNORE_STATIC_ASSETS -eq 0 ]; then
   echo "Copy files to remote host"
   # 3. rsync latest to latest folder on remote host
-  rsync -avz _site/ "webfactional:$RELEASES_DIR/latest"
-
-  echo "Split out latest public files on remove host and save latest as version on remote host"
-  # 4. rsync latest to latest folder on remote host and make new version dir on remote host
-  ssh -t webfactional "rsync -avz $RELEASES_DIR/latest/public/ $RELEASES_DIR/latest_public; mkdir $RELEASES_DIR/$VERSION; cp -r $RELEASES_DIR/latest/* $RELEASES_DIR/$VERSION"
-fi
-
-if [ $DEPLOY_LATEST -eq 0 ]; then
-  echo "rsync web app to latest and rsync web app public to latest public folder"
-  ssh -t webfactional "rsync -avz $RELEASES_DIR/latest/ $WEBAPPS_DIR/ianbyersceramics; rsync -avz $RELEASES_DIR/latest_public $WEBAPPS_DIR/ianbyersceramics_public"
+  rsync -avz  _site/public "webfactional:$WEBAPPS_DIR/ianbyersceramics_public"
 fi
