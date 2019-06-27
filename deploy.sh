57ef060b6e97d50263d93f6c0939ce87b8278823
tombye <tombaromba@gmail.com>
2019-06-27 10:51:49 +0100

    Make deployment configurable
    Now allows you to just deploy changes to static
    assets or the webapp code.

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,13 @@ usage() {
   Deploy a new version to the remote host
 
   OPTIONS:
-    -i Ignore static assets
+    -s just deploy static assets
+    -w just deploy webapp
 EOF
 }
 
-IGNORE_STATIC_ASSETS=0
+JUST_STATIC_ASSETS=0
+JUST_WEBAPP=0
 
 while getopts "hd" OPTION; do
   case $OPTION in
@@ -18,8 +20,11 @@ while getopts "hd" OPTION; do
       usage
       exit 1
       ;;
-    i )
-      IGNORE_STATIC_ASSETS=1
+    s )
+      JUST_STATIC_ASSETS=1
+      ;;
+    w )
+      JUST_WEBAPP=1
       ;;
   esac
 done
@@ -36,12 +41,14 @@ echo "Building static site."
 # 2. build project
 bundle exec jekyll build
 
-echo "Copy files to remote host"
-# 3. rsync latest to latest folder on remote host
-rsync -avz --exclude _site/public _site/ "webfactional:$WEBAPPS_DIR/ianbyersceramics"
-
-if [ $IGNORE_STATIC_ASSETS -eq 0 ]; then
-  echo "Copy files to remote host"
-  # 3. rsync latest to latest folder on remote host
+if [ $JUST_WEBAPP -eq 0 ]; then
+  echo "Copy static asset files to remote host"
+  # 3. rsync static assets to public folder on remote host
   rsync -avz  _site/public "webfactional:$WEBAPPS_DIR/ianbyersceramics_public"
+fi
+
+if [ $JUST_STATIC_ASSETS -eq 0 ]; then
+  echo "Copy webapp files to remote host"
+  # 3. rsync latest version of webapp to relevant folder on remote host
+  rsync -avz --exclude _site/public _site/ "webfactional:$WEBAPPS_DIR/ianbyersceramics"
 fi
